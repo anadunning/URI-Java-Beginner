e3510aaa55b63b3addc368b713ecf5a72b19efde
anadunning <anakpedrosa@hotmail.com>
2020-07-22 15:13:56 -0300

    Simple Sum

---
+++ 1003/src/Beginner1003.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Beginner1003 {
+
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+
+        int a = read.nextInt();
+        int b = read.nextInt();
+
+        System.out.println("SOMA = " +(a+b));
+        System.out.println("\n");
+
+        read.close();
+    }
+}
